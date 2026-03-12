d9ff2d91e29979e098141bee7e5c9a09ea2a8256
Enzo D'Adderio <enzotrade11@gmail.com>
2026-03-12 12:21:43 +0100

    seo: switch to native Next.js sitemap.ts and remove next-sitemap

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://reperefuite.fr',
-  generateRobotsTxt: false,
-  changefreq: 'weekly',
-  priority: 0.7,
-  sitemapSize: 5000,
-}
